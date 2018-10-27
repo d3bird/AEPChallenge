6e72a1d32088916ccbaa0d704269923410198bd8
Scott Crawford <dogbird9@gmail.com>
2018-10-27 12:40:24 -0400

    switched to java, added teh source code to be compleated

---
+++ excelInput.java
@@ -0,0 +1,16 @@
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+import org.apache.poi.sl.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class excelInput {
+
+	/ private static final String FILE_NAME = "/tmp/MyFirstExcel.xlsx";
+	
+	
+}
